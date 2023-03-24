eb4daf821216f6abc5ad863f5a5162a6512eeb69
Irving-lanx <123225827+Irving-lanx@users.noreply.github.com>
2023-03-24 15:53:48 -0600

    Add Javascript file pract.js

---
+++ pract.js
@@ -0,0 +1,42 @@
+//Basic variables
+console.log("Hola mundo1")
+const myInt = 5;
+const myFloat = 6.667;
+console.log(typeof(myInt));
+console.log(typeof(myFloat));
+
+//Fixed number of decimals for floating types
+const lotsOfDecimal = 1.766584958675746364;
+console.log(lotsOfDecimal);
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log(twoDecimalPlaces);
+
+//Converting to number data types
+let myNumber = "74";
+myNumber += 3;
+console.log(myNumber)
+console.log(typeof(myNumber))
+let myNumber2 = "74";
+myNumber2 = Number(myNumber2) + 3;
+console.log(myNumber2%2)
+
+//strings
+const bigmouth = 'I\'ve got no right to take my place…';
+console.log(bigmouth);
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `First ${one}${two}`;
+console.log(joined); // "Hello, how are you?"
+
+/*const button = document.querySelector("button");
+            
+function greet() {
+    const name = prompt("What is your name?");
+    alert(`Hello ${name}, nice to see you!`);
+}
+
+button.addEventListener("click", greet);*/
+
+function showMessage() {
+    alert("Hello JS!");
+}
